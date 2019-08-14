fd959e959e195aaeb76b32da1809ffc0d05134ba
Paolo Sciarra <paolo.sciarra@lastminute.com>
2019-08-14 15:06:46 +0200

    Coverage 100%

--- src/test/kotlin/fpmax/GuessingNumberAppKtTest.kt
+++ src/test/kotlin/fpmax/GuessingNumberAppKtTest.kt
@@ -11,7 +11,7 @@ import java.nio.charset.Charset
 class GuessingNumberAppKtTest {
 
     @Test
-    fun goldenMaster() {
+    fun failAndExit() {
         val charset = Charset.forName("utf-8")
 
         val bao = ByteArrayOutputStream()
@@ -26,5 +26,40 @@ class GuessingNumberAppKtTest {
 
         assertThat(bao.toString(charset), equalTo("What is your name?\nHello, Paolo, welcome to the game!\nDear Paolo, please guess a number from 1 to 5:\nYou guessed wrong, Paolo! The number was: 2\nDo you want to continue, Paolo?\n"))
 
+    }
+
+    @Test
+    fun failAndContinue() {
+        val charset = Charset.forName("utf-8")
+
+        val bao = ByteArrayOutputStream()
+
+        val inputs = listOf("Paolo", "4", "y", "3", "n")
+
+        System.setIn(inputs.joinToString("\n").byteInputStream(charset))
+
+        System.setOut(PrintStream(bao))
+
+        guess { 2 }
+
+        assertThat(bao.toString(charset), equalTo("What is your name?\nHello, Paolo, welcome to the game!\nDear Paolo, please guess a number from 1 to 5:\nYou guessed wrong, Paolo! The number was: 2\nDo you want to continue, Paolo?\nDear Paolo, please guess a number from 1 to 5:\nYou guessed wrong, Paolo! The number was: 2\nDo you want to continue, Paolo?\n"))
+
+    }
+
+    @Test
+    fun pass() {
+        val charset = Charset.forName("utf-8")
+
+        val bao = ByteArrayOutputStream()
+
+        val inputs = listOf("Paolo", "2", "n")
+
+        System.setIn(inputs.joinToString("\n").byteInputStream(charset))
+
+        System.setOut(PrintStream(bao))
+
+        guess { 2 }
+
+        assertThat(bao.toString(charset), equalTo("What is your name?\nHello, Paolo, welcome to the game!\nDear Paolo, please guess a number from 1 to 5:\nYou guessed right, Paolo!\nDo you want to continue, Paolo?\n"))
     }
 }
