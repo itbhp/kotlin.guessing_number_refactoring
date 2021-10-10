b6c801bd6336e6d8b7015e5a1b54a0fe0a99d4e7
Paolo Sciarra <itbhps@gmail.com>
2021-10-10 21:34:19 +0200

    just reformat

--- src/main/kotlin/fpmax/GuessingNumberApp.kt
+++ src/main/kotlin/fpmax/GuessingNumberApp.kt
@@ -3,36 +3,36 @@ package fpmax
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
-        println("What is your name?")
+  println("What is your name?")
 
-        val name = readLine()
+  val name = readLine()
 
-        println("Hello, $name, welcome to the game!")
+  println("Hello, $name, welcome to the game!")
 
-        var exec = true
+  var exec = true
 
-        while (exec) {
-            val num = Random(seed(args)).nextInt(5) + 1
+  while (exec) {
+    val num = Random(seed(args)).nextInt(5) + 1
 
-            println("Dear $name, please guess a number from 1 to 5:")
+    println("Dear $name, please guess a number from 1 to 5:")
 
-            val guess = readLine()?.toInt()
+    val guess = readLine()?.toInt()
 
-            if (guess == num) println("You guessed right, $name!")
-            else println("You guessed wrong, $name! The number was: $num")
+    if (guess == num) println("You guessed right, $name!")
+    else println("You guessed wrong, $name! The number was: $num")
 
-            println("Do you want to continue, $name?")
+    println("Do you want to continue, $name?")
 
-            when (readLine()) {
-                "y" -> exec = true
-                "n" -> exec = false
-            }
-        }
+    when (readLine()) {
+      "y" -> exec = true
+      "n" -> exec = false
     }
+  }
+}
 
 private fun seed(args: Array<String>): Int {
-    if(args.isNotEmpty()){
-        return Integer.valueOf(args[0])
-    }
-    return 5
+  if (args.isNotEmpty()) {
+    return Integer.valueOf(args[0])
+  }
+  return 5
 }
