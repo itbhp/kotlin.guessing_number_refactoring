1c7ef85e7bc75e5c82b0344e96748413d58f35f1
Paolo Sciarra <itbhps@gmail.com>
2021-10-10 21:44:47 +0200

    minor changes

--- src/main/kotlin/fpmax/GuessingNumberApp.kt
+++ src/main/kotlin/fpmax/GuessingNumberApp.kt
@@ -4,29 +4,29 @@ import kotlin.random.Random
 
 
 fun main(args: Array<String>) {
-        println("What is your name?")
+  println("What is your name?")
 
-        val name = readLine()
+  val name = readLine()
 
-        println("Hello, $name, welcome to the game!")
+  println("Hello, $name, welcome to the game!")
 
-        var exec = true
+  var exec = true
 
-        while (exec) {
-          val num = Random.nextInt(5) + 1
+  while (exec) {
+    val num = Random.nextInt(5) + 1
 
-          println("Dear $name, please guess a number from 1 to 5:")
+    println("Dear $name, please guess a number from 1 to 5:")
 
-          val guess = readLine()?.toInt()
+    val guess = readLine()?.toInt()
 
-          if (guess == num) println("You guessed right, $name!")
-          else println("You guessed wrong, $name! The number was: $num")
+    if (guess == num) println("You guessed right, $name!")
+    else println("You guessed wrong, $name! The number was: $num")
 
-          println("Do you want to continue, $name?")
+    println("Do you want to continue, $name?")
 
-          when (readLine()) {
-            "y" -> exec = true
-            "n" -> exec = false
-          }
-        }
+    when (readLine()) {
+      "y" -> exec = true
+      "n" -> exec = false
     }
+  }
+}
